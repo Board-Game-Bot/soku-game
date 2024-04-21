ecd6715c1b2dfc77578b1c1ff73e1f0b6f420a05
SokuRitszZ <1062708758@qq.com>
2024-04-21 18:06:27 +0800

    chore: update core dev

--- packages/core/rollup.config.ts
+++ packages/core/rollup.config.ts
@@ -1,19 +1,19 @@
 import { defineConfig } from 'rollup';
-import ts from 'rollup-plugin-typescript2';
+import ts from '@rollup/plugin-typescript';
 import dts from 'rollup-plugin-dts';
 
-export default defineConfig([
-  {
-    input: './src/core.ts',
-    output: [
-      { file: './dist/index.cjs', format: 'cjs' },
-      { file: './dist/index.mjs', format: 'esm' },
-    ],
-    plugins: [ts()],
+export default defineConfig([{
+  input: 'src/index.ts',
+  output: [
+    { file: 'dist/index.cjs', format: 'cjs' },
+    { file: 'dist/index.mjs', format: 'esm' },
+  ],
+  plugins: [ts()],
+}, {
+  input: 'src/index.ts',
+  output: {
+    file: 'dist/index.d.ts',
+    format: 'es',
   },
-  {
-    input: './src/core.ts',
-    output: [{ file: './dist/index.d.ts', format: 'es' }],
-    plugins: [dts()],
-  },
-]);
+  plugins: [dts()],
+}]);
